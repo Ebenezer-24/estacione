c9294668fd22df13cc656254cd404106f86ab7b5
juan <ebenalzogaray@gmail.com>
2024-07-20 00:59:18 -0300

    Update UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -51,24 +51,31 @@ class UsuarioController extends Controller
         return view('usuarios.edit', compact('usuario'));
     }
 
-    public function update(Request $request, Usuario $usuario)
+    public function update(Request $request,Usuario $usuario, $dni)
     {
         $request->validate([
-            'nombre' => 'required',
-            'apellido' => 'required',
-            'domicilio' => 'required',
-            'email' => 'required|email|unique:usuarios,email,' . $usuario->id,
+            'nombre' => 'required|string|max:255',
+            'apellido' => 'required|string|max:255',
+            'domicilio' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:usuarios,email,' . $dni . ',dni',
             'fecha_nacimiento' => 'required|date',
             'patente' => 'required|unique:usuarios,patente,' . $usuario->id,
+            'password' => 'sometimes|string|min:8',
+            
         ]);
-
         $usuario->update($request->except('password'));
+        $usuario = Usuario::findOrFail($dni);
+        $usuario->nombre = $request->nombre;
+        $usuario->apellido = $request->apellido;
+        $usuario->domicilio = $request->domicilio;
+        $usuario->email = $request->email;
+        $usuario->fecha_nacimiento = $request->fecha_nacimiento;
+        $usuario->patente_vehiculo = $request->patente_vehiculo;
 
         if ($request->filled('password')) {
             $usuario->password = bcrypt($request->password);
             $usuario->save();
         }
-
         return redirect()->route('usuarios.index');
     }
 
